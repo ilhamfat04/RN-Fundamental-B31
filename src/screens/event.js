763c874dbfc66b6c6adf2a6b97dd8b0c3a82edf4
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-07-31 01:47:27 +0700

    3.Event

--- src/screens/event.js
+++ src/screens/event.js
@@ -2,10 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
 
-export default function EmbedExpression() {
+export default function Event() {
   // Create Function Here
-
-  // Create a variable Here
 
   return (
     <View>
