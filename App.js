f7431c4fb9028f698c9c0ad37c54b22accdfbf4e
= <=>
2022-03-29 15:17:21 +0700

    update

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
+import Map from "./src/screens/map";
 
 export default function App() {
   return (
-    <View>
+    <View style={{ marginTop: 200 }}>
       <StatusBar />
       {/* Use Component */}
+      <Map />
     </View>
   );
 }
--- src/screens/map.js
+++ src/screens/map.js
@@ -1,14 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function Map() {
   // Create Dummy Data With Array
+  const cars = ["BMW", "Mercedes", "Bugati", "Jaguar"]
 
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
+      <Text>This is List on RN</Text>
+      {cars.map((car) => (
+        <Text style={{ color: "red" }} key={car}>{car}</Text>
+      ))}
     </View>
   );
 }
