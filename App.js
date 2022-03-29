51a2bd9828308559ac22a3fbde41f737aa0faff0
= <=>
2022-03-29 14:22:50 +0700

    update

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
+import Event from "./src/screens/event";
 
 export default function App() {
   return (
-    <View>
+    <View style={{ marginTop: 200 }}>
       <StatusBar />
       {/* Use Component */}
+      <Event />
     </View>
   );
 }
--- src/screens/event.js
+++ src/screens/event.js
@@ -1,14 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default function Event() {
   // Create Function Here
+  function Greeting() {
+    return alert("Hello batch 31 fullstack")
+  }
 
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
+      <Text>If you pressed Click Here then the alert will appear</Text>
+      <TouchableOpacity onPress={Greeting} >
+        <Text>Click Here</Text>
+      </TouchableOpacity>
     </View>
   );
 }
