10f318565ba9be27e231b7d4665b0acf12d88a92
= <=>
2022-03-29 15:30:44 +0700

    update

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
+import List from "./src/screens/flatList";
 
 export default function App() {
   return (
-    <View>
+    <View style={{ marginTop: 100 }}>
       <StatusBar />
       {/* Use Component */}
+      <List />
     </View>
   );
 }
--- src/screens/flatList.js
+++ src/screens/flatList.js
@@ -1,13 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, FlatList, Text } from "react-native";
 
 export default function List() {
   // Make Dummy Data with Array
+
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
+      <Text>This is List on RN using FlatList</Text>
+      <FlatList
+        data={[
+          {
+            name: "Ilham Fathullah"
+          },
+          {
+            name: "Jody Septiawan"
+          },
+          {
+            name: "Samsul Rijal"
+          },
+        ]}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+        keyExtractor={(item) => item.name}
+      />
     </View>
   );
 }
