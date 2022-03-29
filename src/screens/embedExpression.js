ac8583b98b4c5d2503d64a1e81b465129005d438
= <=>
2022-03-29 14:13:29 +0700

    update

--- src/screens/embedExpression.js
+++ src/screens/embedExpression.js
@@ -1,16 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function EmbedExpression() {
   // Create Function Here
+  function getMajor() {
+    return "Fullstack Batch 31"
+  }
 
   // Create a variable Here
+  const companyName = "Dumbways.ID"
 
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
+      <Text>
+        Welcome to {companyName} Class {getMajor()}
+      </Text>
     </View>
   );
 }
