6f5e25e790015aba5f5111349182af615262fa2e
= <=>
2022-03-29 14:45:50 +0700

    update

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
+import State from "./src/screens/state";
 
 export default function App() {
   return (
-    <View>
+    <View style={{ marginTop: 200 }}>
       <StatusBar />
       {/* Use Component */}
+      <State />
     </View>
   );
 }
--- src/screens/state.js
+++ src/screens/state.js
@@ -1,18 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default function State() {
   // Init State
+  const [counter, setCounter] = useState(0)
 
   //Create Function Add
+  function Add() {
+    return setCounter(counter + 1)
+  }
 
   //Create Function Less
+  function Less() {
+    return setCounter(counter - 1)
+  }
 
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
+      <Text>If you click the add button, it will increase. But if you click less button its opposite from adding.</Text>
+
+      <Text>{counter}</Text>
+
+      <TouchableOpacity onPress={Add}>
+        <Text>Add</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={Less}>
+        <Text>Less</Text>
+      </TouchableOpacity>
     </View>
   );
 }
